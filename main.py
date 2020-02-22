c9b04ffddb952968919a89b7d6adc2d46196d377
acp21 <57569721+acp21@users.noreply.github.com>
2020-02-22 13:58:38 -0500

    Update Works

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import time
+
 import kivy
 import requests
 import datetime
 import json
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
@@ -33,17 +36,28 @@ class Home(App):
         timeString = date.strftime("%I:%M %p")
         dateString = date.strftime("%B %d, %Y")
         tempString = "It is currently " + str(temp) + " degrees"
-        temperature = Label(text=tempString, size_hint=(.6, .6), pos=(150, 900), font_size=30)
-        temperature.size_hint = (0.1, 0.1)
-        time = Label(text=timeString, size_hint=(.6, .6), pos=(1700, 900), font_size=30)
-        time.size_hint = (0.1, 0.1)
-        day = Label(text=dateString, size_hint=(.6, .6), pos=(1650, 850), font_size=30)
-        day.size_hint = (0.1, 0.1)
-        layout.add_widget(day)
-        layout.add_widget(time)
-        layout.add_widget(temperature)
+        self.temperature = Label(text=tempString, size_hint=(.6, .6), pos=(150, 900), font_size=30)
+        self.temperature.size_hint = (0.1, 0.1)
+        self.time = Label(text=timeString, size_hint=(.6, .6), pos=(1700, 900), font_size=30)
+        self.time.size_hint = (0.1, 0.1)
+        self.day = Label(text=dateString, size_hint=(.6, .6), pos=(1650, 850), font_size=30)
+        self.day.size_hint = (0.1, 0.1)
+        layout.add_widget(self.day)
+        layout.add_widget(self.time)
+        layout.add_widget(self.temperature)
+        t = 5.0   # 1/60th of a second, use 1.0 instead to update every second
+        Clock.schedule_interval(self.update, t)
         return layout
+
+    def update(self, dt):
+        temp, weather = getWeather()
+        temp = round((temp - 273) * 9 / 5 + 32)
+        date = datetime.datetime.now()
+        timeString = date.strftime("%I:%M %p")
+        dateString = date.strftime("%B %d, %Y")
+        self.temperature.text = "It is currently " + str(temp) + " degrees"
+        self.time.text = timeString
+        self.day.text = dateString
 
 if __name__ == "__main__":
     Home().run()
-    Home.time.text("Big gay")
